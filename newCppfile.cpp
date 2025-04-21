461c631666f6c3a39b4b1f3465c6efc15106f1e6
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-04-21 20:07:30 +0330

    Update newCppfile.cpp

--- newCppfile.cpp
+++ newCppfile.cpp
@@ -1,15 +1,11 @@
 #include<iostream>
 using namespace std;
-void printStudentID() {
-    cout << "Student ID: 4033623022" << endl;
-}
 void printFunction()
 {
     cout << "Hello World"<<endl;
 }
 int main()
 {
-    printStudentID;
     printFunction;
     return 0;
 }
