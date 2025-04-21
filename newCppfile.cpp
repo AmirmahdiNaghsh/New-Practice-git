5365301d54a9842a222fdafc5cd87f93ef634ff3
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-04-21 19:54:37 +0330

    Second Commit

--- newCppfile.cpp
+++ newCppfile.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
+void printStudentID() {
+    cout << "Student ID: 4033623022" << endl;
+}
 void printFunction()
 {
     cout << "Hello World"<<endl;
 }
 int main()
 {
+    printStudentID;
     printFunction;
     return 0;
 }
